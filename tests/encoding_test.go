9c0ced36b651e7cd78872037016b05bfd0d1af11
xixi <i@hexilee.me>
2022-11-09 23:27:55 +0800

    testFile fails
    Signed-off-by: xixi <i@hexilee.me>

--- tests/encoding_test.go
+++ tests/encoding_test.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"log"
 	"math/rand"
+	"os"
 	"testing"
 	"time"
 
@@ -21,15 +22,22 @@ import (
 var (
 	Dispatcher  = iorpc.NewDispatcher()
 	ServiceEcho iorpc.Service
+	ServiceFile iorpc.Service
 
 	headerSize = binary.Size(RequestHeaders{})
 )
 
 type buffer []byte
+
 type RequestHeaders struct {
 	Key uint64
 }
+
 type DataHash string
+
+type File struct {
+	file *os.File
+}
 
 func (h *RequestHeaders) Encode(w io.Writer) (int, error) {
 	return headerSize, binary.Write(w, binary.BigEndian, *h)
@@ -47,6 +55,18 @@ func (h *DataHash) Encode(w io.Writer) (int, error) {
 
 func (h *DataHash) Decode(data []byte) error {
 	*h = DataHash(data)
+	return nil
+}
+
+func (f *File) File() uintptr {
+	return f.file.Fd()
+}
+
+func (f *File) Read(p []byte) (n int, err error) {
+	return f.file.Read(p)
+}
+
+func (f *File) Close() error {
 	return nil
 }
 
@@ -80,6 +100,43 @@ func init() {
 				Body: iorpc.Body{
 					Size:   uint64(len(hash)),
 					Reader: &reader,
+				},
+			}, nil
+		},
+	)
+
+	data, err := os.ReadFile("./tmp/testdata")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.Open("./tmp/testdata")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	size := stat.Size()
+
+	ServiceFile, _ = Dispatcher.AddService(
+		"File",
+		func(clientAddr string, request iorpc.Request) (*iorpc.Response, error) {
+			_ = request.Body.Close()
+			header, ok := request.Headers.(*RequestHeaders)
+			if !ok {
+				return nil, errors.New("invalid header")
+			}
+
+			hash := sum(header.Key, data)
+			str := DataHash(hash)
+			return &iorpc.Response{
+				Headers: &str,
+				Body: iorpc.Body{
+					Size:   uint64(size),
+					Reader: &File{file: file},
 				},
 			}, nil
 		},
@@ -156,60 +213,8 @@ func TestEncoding(t *testing.T) {
 		client := NewClient(addr, i)
 		defer client.Stop()
 		for j := 0; j < 20; j++ {
-			eg.Go(func() error {
-				for {
-					select {
-					case <-ctx.Done():
-						return ctx.Err()
-					default:
-						err := func() error {
-							key := rand.Uint64()
-							data := randomData(1024)
-							hash := sum(key, data)
-							reader := buffer(data)
-
-							req := iorpc.Request{
-								Service: ServiceEcho,
-								Headers: &RequestHeaders{
-									Key: key,
-								},
-								Body: iorpc.Body{
-									Size:   uint64(len(data)),
-									Reader: &reader,
-								},
-							}
-							resp, err := client.Call(req)
-							if err != nil {
-								return errors.Wrap(err, "call")
-							}
-							defer resp.Body.Close()
-
-							h, ok := resp.Headers.(*DataHash)
-							if !ok {
-								return errors.New("invalid header")
-							}
-
-							if DataHash(hash) != *h {
-								return fmt.Errorf("invalid hash: %s != %s", hash, *h)
-							}
-
-							respData, err := io.ReadAll(resp.Body.Reader)
-							if err != nil {
-								return errors.Wrap(err, "read body")
-							}
-
-							if !bytes.Equal(respData, hash) {
-								return fmt.Errorf("invalid data: %s != %s", hash, respData)
-							}
-							return nil
-						}()
-
-						if err != nil {
-							return err
-						}
-					}
-				}
-			})
+			eg.Go(testEcho(ctx, client))
+			eg.Go(testFile(ctx, client))
 		}
 	}
 
@@ -218,4 +223,107 @@ func TestEncoding(t *testing.T) {
 		err = nil
 	}
 	a.Nil(err)
+}
+
+func testEcho(ctx context.Context, client *iorpc.Client) func() error {
+	return func() error {
+		for {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			default:
+				err := func() error {
+					key := rand.Uint64()
+					data := randomData(1024)
+					hash := sum(key, data)
+					reader := buffer(data)
+
+					req := iorpc.Request{
+						Service: ServiceEcho,
+						Headers: &RequestHeaders{
+							Key: key,
+						},
+						Body: iorpc.Body{
+							Size:   uint64(len(data)),
+							Reader: &reader,
+						},
+					}
+					resp, err := client.Call(req)
+					if err != nil {
+						return errors.Wrap(err, "call")
+					}
+					defer resp.Body.Close()
+
+					h, ok := resp.Headers.(*DataHash)
+					if !ok {
+						return errors.New("invalid header")
+					}
+
+					if DataHash(hash) != *h {
+						return fmt.Errorf("invalid hash: %s != %s", hash, *h)
+					}
+
+					respData, err := io.ReadAll(resp.Body.Reader)
+					if err != nil {
+						return errors.Wrap(err, "read body")
+					}
+
+					if !bytes.Equal(respData, hash) {
+						return fmt.Errorf("invalid data: %s != %s", hash, respData)
+					}
+					return nil
+				}()
+
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+}
+
+func testFile(ctx context.Context, client *iorpc.Client) func() error {
+	return func() error {
+		for {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			default:
+				err := func() error {
+					key := rand.Uint64()
+					req := iorpc.Request{
+						Service: ServiceFile,
+						Headers: &RequestHeaders{
+							Key: key,
+						},
+					}
+					resp, err := client.Call(req)
+					if err != nil {
+						return errors.Wrap(err, "call")
+					}
+					defer resp.Body.Close()
+
+					respData, err := io.ReadAll(resp.Body.Reader)
+					if err != nil {
+						return errors.Wrap(err, "read body")
+					}
+
+					hash := sum(key, respData)
+					h, ok := resp.Headers.(*DataHash)
+					if !ok {
+						return errors.New("invalid header")
+					}
+
+					if !bytes.Equal([]byte(*h), hash) {
+						return fmt.Errorf("invalid data: %x != %x", []byte(*h), hash)
+					}
+					return nil
+				}()
+
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
 }
